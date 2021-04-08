789843ecfb539503185a8bcd4fad0a797e1afe88
Dranatom <zorg.anatom1990@gmail.com>
2021-04-08 14:02:51 +0300

    add new function

--- bin/main.dart
+++ bin/main.dart
@@ -2,31 +2,40 @@ void main(List<String> arguments) {
   print(chooseBestDistance(174, 3, [51, 56, 58, 59, 61])); //173
   print(chooseBestDistance(163, 3, [50])); // -1
 }
+
 int chooseBestDistance(int t, int k, List<int> ls) {
-  if(ls.length < k){
+  if (ls.length < k) {
     return -1;
   }
-  var distanceBetweenThreeCities = <int>[];
+  var distanceBetweenCities = <int>[];
   var result = -1;
   var limitMaxDistance = t;
-  for (var i = 0; i < ls.length; i++) {
-        distanceBetweenThreeCities.add(calculateDistance(ls[i], k -1, ls.sublist(i+1, ls.length)));
-  }
-  for (var i = 0; i < distanceBetweenThreeCities.length; i++) {
-    if(distanceBetweenThreeCities[i] <= t){
-      if(t - distanceBetweenThreeCities[i] < limitMaxDistance){
-        result = distanceBetweenThreeCities[i];
-        limitMaxDistance = t - distanceBetweenThreeCities[i];
+    distanceBetweenCities
+        .addAll(calculateDistance(ls, k, 0, List.filled(k, 0), <int>[]));
+  for (var i = 0; i < distanceBetweenCities.length; i++) {
+    if (distanceBetweenCities[i] <= t) {
+      if (t - distanceBetweenCities[i] < limitMaxDistance) {
+        result = distanceBetweenCities[i];
+        limitMaxDistance = t - distanceBetweenCities[i];
       }
     }
   }
   return result;
 }
-int calculateDistance (int start, int countCity, List<int> dist){
-  var result = start;
-  if(countCity > 0 && dist.length >= countCity) {
-    return calculateDistance (start + dist[0], countCity - 1, dist.sublist(1, dist.length));
+
+List<int> calculateDistance(List<int> arr, int len, int startPosition,
+    List<int> result, List<int> distance) {
+  var temp = 0;
+  if (len == 0) {
+    for (var i = 0; i < result.length; i++) {
+      temp += result[i];
+    }
+    distance.add(temp);
   } else {
-    return result;
+    for (var i = startPosition; i <= arr.length - len; i++) {
+      result[result.length - len] = arr[i];
+      calculateDistance(arr, len - 1, i + 1, result, distance);
+    }
   }
+  return distance;
 }
